51edb43c32b5b828b7d66da4d44df56ff3638729
Mukul Rawat <mukulsrawat@gmail.com>
2016-07-24 22:44:25 +0530

    updated main program to search and read issues

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"bufio"
 	"fmt"
 	"io"
+	"log"
 	"os"
 )
 
@@ -17,7 +18,17 @@ var (
 
 // Run_ReadIssue runs the functions to Read Issues from the command line and prints them out
 func Run_ReadIssues(in stringReader, out io.Writer) {
-	fmt.Fprintf(out, "Hello World!")
+
+	IO_ReadIssues(in)
+	result, err := ReadIssues(Query)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Printf("%d issues:\n", result.TotalCount)
+	for _, item := range result.Items {
+		fmt.Printf("#%-5d %9.9s %.55s\n", item.Number, item.User.Login, item.Title)
+	}
 }
 
 func main() {
