a47e1b43d7197e751b6a02f478e8bdff12bd6bc6
Mukul Rawat <mukulsrawat@gmail.com>
2016-07-25 22:23:55 +0530

    added a simple framework for a cli app

---
+++ main.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"os"
+	"github.com/urfave/cli"
+)
+
+func main() {
+	app := initApp()
+	app.Run(os.Args)
+}
+
+func initApp() *cli.App {
+	
+	// Initiaize the new app
+	app := cli.NewApp()
+	
+	app.Name = "ghi"
+	app.Version = "0.0.1"
+	app.Usage = "Github Issues on the command line"
+	app.Author = "Mukul Rawat"
+	app.Email = "mukulsrawat@gmail.com"
+	
+	app.Commands = []cli.Command {
+		listIssuesCommand(),
+	}
+	return app
+}
+
+func listIssuesCommand() cli.Command {
+	command := cli.Command {
+		Name: "list",
+		Usage: "List the issues of the mentioned repository",
+		Action: runListIssues,
+	}
+	return command
+}
